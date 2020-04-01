0252552bbc27a8a64df97f3fc43de67e6da9eb89
Adam Khalepo <adam.khalepo@iut-rodez.fr>
2020-04-01 17:34:38 +0200

    fix #4 fonction P2-2-1 fonction find_activities_older_than

---
+++ P2-2-1-find_activities_older_than.sql
@@ -0,0 +1,4 @@
+CREATE FUNCTION find_activities_older_than (old_date date) RETURNS activity AS $$
+    SELECT title FROM activity WHERE old_date = creation_date
+    RETURNING activity
+$$ LANGUAGE SQL;
