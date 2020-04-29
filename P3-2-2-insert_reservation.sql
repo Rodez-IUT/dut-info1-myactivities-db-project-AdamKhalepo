a93fad304eae6aea9b67e6e2daf5d6ec9f06fa84
Adam Khalepo <adam.khalepo@iut-rodez.fr>
2020-04-29 14:12:02 +0200

    fix #7 fonction P3-2-2 fonction insert_reservation

--- P3-2-2-insert_reservation.sql
+++ P3-2-2-insert_reservation.sql
@@ -1,7 +1,25 @@
-CREATE FUNCTION gds.insert_reservation(une_date_debut timestamp without time zone,une_date_fin timestamp without time zone,un_nom_de_salle character varying(250)) RETURNS reservation AS $$
+-- Fonction recuperant l'id d'une salle à partir de son nom
 
-INSERT INTO reservation (une_date_debut, une_date_fin, un_nom_de_salle)
-	VALUES	
-	(nextval('gds.reservation_seq'),now(),now(),un_nom_de_salle); --modifier un_nom_de_salle, mettre autre chose car faux
-	RETURNING reservation;
+CREATE OR REPLACE FUNCTION gds.get_salle_id(un_nom_de_salle character varying(250)) 
+RETURNS bigint AS $$
+	select id from gds.salle where nom = un_nom_de_salle;
+$$ LANGUAGE SQL;
+
+-- Fonction insérant une nouvelle réservation
+
+CREATE OR REPLACE FUNCTION gds.insert_reservation(
+    une_date_debut timestamp without time zone,
+    une_date_fin timestamp without time zone, 
+    un_nom_de_salle character varying(250) 
+) RETURNS gds.reservation AS $$
+        -- insert la reservation
+        INSERT INTO gds.reservation (id, date_debut, date_fin, date_creation, date_modification,salle_id) 
+        VALUES (nextval('gds.reservation_seq'),
+				une_date_debut, 
+				une_date_fin, 
+				now(), 
+				now(),
+				gds.get_salle_id(un_nom_de_salle)
+			   ) 
+        returning *;
 $$ LANGUAGE SQL;
