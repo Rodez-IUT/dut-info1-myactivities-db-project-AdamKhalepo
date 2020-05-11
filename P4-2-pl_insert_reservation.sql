13b65da087e9b32a27c9db5261c4731af0b8ef99
Adam Khalepo <adam.khalepo@iut-rodez.fr>
2020-05-11 20:57:50 +0200

    fix #10 fonction pl_insert_reservation

--- P4-2-pl_insert_reservation.sql
+++ P4-2-pl_insert_reservation.sql
@@ -1,17 +1,28 @@
-CREATE OR REPLACE FUNCTION gds.pl_insert_reservation (une_date_debut timestamp without time zone, une_date_fin timestamp without time zone, un_nom_de_salle varchar(250)) RETURNS gds.reservation AS $$
-DECLARE 
-    une_reservation gds.reservation%ROWTYPE;
-    un_numero_de_salle bigint;
-BEGIN
-    SELECT id INTO un_numero_de_salle FROM gds.salle WHERE nom = un_nom_de_salle;
-    IF FOUND 
-        THEN 
-            INSERT INTO gds.reservation (date_debut,date_fin,date_creation,date_modification,id,salle_id)
-            VALUES (une_date_debut,une_date_fin,now(),now(),nextval('gds.reservation_seq'),un_numero_de_salle)
-            RETURNING * INTO une_reservation;
-        ELSE
-            RAISE NOTICE 'La salle est indisponible ou est inexistante' ;
-    END IF;
-    RETURN une_reservation;
-END;
-$$ LANGUAGE plpgsql
+CREATE OR REPLACE FUNCTION gds.pl_insert_reservation(
+    une_date_debut timestamp without time zone,
+    une_date_fin timestamp without time zone, 
+    un_nom_de_salle character varying(250) 
+) RETURNS gds.reservation AS $$
+	DECLARE
+		salle_id gds.salle.id%TYPE;
+		reservation gds.reservation%ROWTYPE;
+	BEGIN
+		SELECT id INTO salle_id from gds.salle where nom = un_nom_de_salle;
+		IF FOUND 
+		THEN
+			-- insert la reservation
+        	INSERT INTO gds.reservation (id, date_debut, date_fin, date_creation, date_modification,salle_id) 
+        		VALUES (nextval('gds.reservation_seq'),
+					une_date_debut, 
+					une_date_fin, 
+					now(), 
+					now(),
+					salle_id
+			   ) 
+        	returning * INTO reservation;
+		ELSE
+			RAISE NOTICE 'La salle demandée n''existe pas';
+		END IF;
+	RETURN reservation;
+	END;
+$$ LANGUAGE plpgsql;
